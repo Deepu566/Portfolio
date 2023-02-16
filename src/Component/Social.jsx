f3c9f0c9d87ac54b604a3df8d74ff6b79f8a15ac
Deepu566 <deepu566255@gmail.com>
2023-02-16 16:01:17 +0530

    c

--- src/Component/Social.jsx
+++ src/Component/Social.jsx
@@ -8,16 +8,16 @@ const Social = (props) => {
     return (
         <div className='max-sm:text-2xl text-white' >
             <footer className='flex gap-5 text-white items-center '>
-                <a href="http://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.instagram.com/deepu5439/" target="_blank" rel="noopener noreferrer">
                     <AiOutlineInstagram />
                 </a>
-                <a href="http://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.facebook.com/profile.php?id=100018032026012" target="_blank" rel="noopener noreferrer">
                     <FaFacebookF />
                 </a>
                 <a href="http://www.twitter.com" target="_blank" rel="noopener noreferrer">
                     <FiTwitter />
                 </a>
-                <a href="http://www.github.com" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/Deepu566" target="_blank" rel="noopener noreferrer">
                     <AiOutlineGithub />
                 </a>
             </footer>
